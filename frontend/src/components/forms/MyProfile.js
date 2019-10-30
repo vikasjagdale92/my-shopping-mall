6a9cf8eb1dcd36f01226c056abd61a4f9c47e7c2
vikasjagdale92 <vikasjagdale92@gmail.com>
2019-10-30 11:28:12 +0530

    vikas added changes after master merge

--- frontend/src/components/forms/MyProfile.js
+++ frontend/src/components/forms/MyProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class MyProfile extends Component {
   render() {
-    return <div>My Profile</div>;
+    return <div>My Profile wow</div>;
   }
 }
 export default MyProfile;
